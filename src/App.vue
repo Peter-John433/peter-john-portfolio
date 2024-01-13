b09618d4b593e9d2bb8fe3a1490d69097db1cf0c
iampeterjohn <peterjeche442@gmail.com>
2024-01-12 13:29:51 -1100

    home section

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,30 @@
 <template>
    <Header />
    <div class="container">
-      
+      <section id="home" class="home">
+         <div class="home-content">
+            <h1>
+                Hi!, I'm PETER JOHN
+            </h1>
+
+            <div>
+               <h3>
+                  And I'm
+               </h3>
+
+               <h3>
+                  <span>Frontend developer</span>
+                  <span>Web designer</span>
+               </h3>
+            </div>
+         </div>
+      </section>
    </div>
 </template>
 
 <style scoped>
-
+  h3, span{
+    color: var(--bg-color2);
+    font-weight: 700;
+  }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,12 +5,12 @@
 <template>
    <div class="gen-container w-full">
        <header class="flex justify-between items-center container">
-           <div class="header-logo text-[26px] font-[700] font-sans">
+           <div class="header-logo text-[26px] font-[700] font-sans text-white">
                <span class="j-logo font-[700]">J</span>ohn
            </div>
 
            
-               <ul class="flex font-[500] header-list">
+               <ul class="block sm:flex  font-[500] header-list">
                  <a href=""><li>Home</li></a>
                  <a href=""><li>About</li></a>
                  <a href=""><li>Services</li></a>
@@ -19,8 +19,8 @@
                  <a href=""><li>Skills</li></a>
                </ul>
           
-           <div>
-            <i class='bx bx-menu '></i>
+           <div class="text-[2rem] mr-[16px] text-[yellow] block sm:hidden">
+            <i class='bx bx-menu'></i>
            </div>
        </header>
    </div>
@@ -39,11 +39,19 @@
      font-weight: 500;
      padding: 20px 20px;
      transition: all 500ms ease-in-out;
+     color: var(--text-white);
   }
 
   .header-list a:hover{
      color: var(--bg-color2);
      background-color: #D0D0D0;
+     /* text-shadow: 0 0 10px rgba(255, 255, 0),
+    0 0 20px rgba(255, 255, 0),
+    0 0 30px rgba(255, 255, 0),
+    0 0 40px rgba(255, 255, 0),
+    0 0 70px rgba(255, 255, 0),
+    0 0 100px rgba(255, 255, 0),
+    0 0 150px rgba(255, 255, 0); */
 
   }
 </style>
