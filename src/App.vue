f645bb520b5e2fef0b9610d264f94dfe14abb653
iampeterjohn <peterjeche442@gmail.com>
2024-01-12 15:34:48 -1100

    update

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
             </h1>
 
             <div class="change-text inline-flex m-0">
-               <h3 class="font-[600] text-[1.5rem] me-3">
+               <h3 class="font-[600] text-[1.5rem] me-3 text-[#D0D0D0]">
                   And I'm
                </h3>
 
@@ -29,18 +29,18 @@
                </h3>
             </div>
 
-               <p class=" leading-6 text-[18px] text-[red]">
+               <p class=" leading-6 text-[18px] text-[yellow]">
                   Frontend developer, Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur culpa corrupti quasi praesentium ipsam, sunt quo cumque vero voluptatem mollitia!
                </p>
            
-               <div class="info-box">
+               <div class="info-box my-3">
                   <h5>Email:</h5>
-                  <span>peterjeche442@gmail.com</span>
+                  <span class="font-bold tracking-wider">peterjeche442@gmail.com</span>
                </div>
 
-               <div class="btn-box">
-                  <a href="">Download CV</a>
-                  <a href="">Hire Me</a>
+               <div class="btn-box flex justify-between mt-[1.5rem] w-[320px] h-[46px]">
+                  <a href="#" class="btn">Download CV</a>
+                  <a href="#" class="btn">Hire Me</a>
                </div>
 
                <div class="social-icon">
@@ -60,10 +60,6 @@
 </template>
 
 <style scoped>
-  h3, span{
-    color: var(--bg-color2);
-    font-weight: 700;
-  }
 
   .home{
    display: grid;
@@ -92,5 +88,55 @@
    position: absolute;
    display: flex;
    opacity: 0;
+ }
+   
+ .btn-box{
+   display: flex; 
+   justify-content: center;
+   align-items: center;
+   column-gap: 2rem;
+   /* width: 400px; */
+   height: 46px;
+ }
+ .btn-box a.btn{
+    width: 150px; 
+    background-color: #01011a;
+    border: 1px solid #d0d0d0;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    color: var(--bg-color2);
+    height: 100%;
+    font-weight: 700;
+    letter-spacing: 1px;
+    font-size: 1rem;
+    transition: .6s;
+    position: relative;
+    overflow: hidden;
+    border-radius: 5px;
+    z-index: 1;
+    color: yellow;
+ }
+
+ .btn-box a.btn:hover{
+   box-shadow: 0 0 2px 2px yellow;
+   color: #01011a;
+   border: 1px solid transparent;
+ }
+
+ a.btn::before{
+   content: '';
+   position: absolute;
+   top: 0;
+   left: 0;
+   width: 0;
+   height: 100%;
+   background-color: yellow;
+   z-index: -1;
+   transition: .4s;
+ }
+
+ a.btn:hover::before{
+   width: 100%;
  }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,7 @@
 <template>
    <div class="gen-container w-full">
        <header class="flex justify-between items-center container">
-           <div class="header-logo text-[26px] font-[700] font-sans text-white">
+           <div class="header-logo text-[26px] font-[700] font-sans text-[#01011a]">
                <span class="j-logo font-[700]">J</span>ohn
            </div>
 
@@ -28,23 +28,26 @@
 
 <style scoped>
   .gen-container{
-      background-color: var(--bg-color2);
+      /* background-color: var(--bg-color2); */
+      background-color: #D0D0D0;
   }
 
   .j-logo{
-    color: var(--logo-text-color);
+    /* color: var(--logo-text-color); */
+    color: red;
   }
 
   .header-list a{
      font-weight: 500;
      padding: 20px 20px;
      transition: all 500ms ease-in-out;
-     color: var(--text-white);
+     color: var(--bg-color2);
   }
 
   .header-list a:hover{
      color: var(--bg-color2);
-     background-color: #D0D0D0;
+     background-color: #01011a;
+     color: yellow;
      /* text-shadow: 0 0 10px rgba(255, 255, 0),
     0 0 20px rgba(255, 255, 0),
     0 0 30px rgba(255, 255, 0),
