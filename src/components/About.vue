55145c5c3ed9480a8eb15068d051fbcefdfd95f5
iampeterjohn <peterjeche442@gmail.com>
2024-01-13 05:05:28 -1100

    update

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,15 +1,15 @@
 <template>
    <div>
         <div id="about" class="about bg-[yellow] rounded-xl mt-20 flex justify-center items-center gap-x-32">
-            <div class="img-about text-center relative">
-                <img src="@/assets/eche3-removebg-preview.png" class=" max-w-[500px] w-full h-[78vh] object-cover rounded-br-full rounded-bl-full" alt="">
+            <div class="img-about text-center relative rounded-full p-0 m-0 ">
+                <img src="@/assets/eche3-removebg-preview.png" class=" max-w-[500px] w-full h-[758px] object-cover rounded-br-full rounded-bl-full" alt="">
             
-                <div class="about-info">
+                <div class="about-info info-1">
                     <span>5+</span>
                     <p>Years of experience</p>
                 </div>
 
-                <div class="about-info">
+                <div class="about-info info-2">
                     <span>100+</span>
                     <p>projects completed</p>
                 </div>
@@ -25,6 +25,14 @@
                 <div class="btn-box">
                     <a href="#" class="btn">Read more</a>
                 </div>
+
+                <div class="social-icon mt-[5rem]">
+                  <a href=""><i class='bx bxl-facebook bx-tada'></i></a>
+                  <a href="#"><i class='bx bxl-instagram-alt bx-tada' ></i></a>
+                  <a href="#"><i class='bx bxl-upwork bx-tada' ></i></a>
+                  <a href="#"><i class='bx bxl-linkedin bx-tada' ></i></a>
+                  <a href="#"><i class='bx bxl-twitter bx-tada' ></i></a>
+               </div>
             </div>
         </div>
    </div>
@@ -83,10 +91,10 @@
  }
 
   .about-content{
-     color: red;
+     color: #01011a;
      font-size: 1.5rem;
      width: 600px;
-     font-weight: 500;
+     font-weight: 700;
      display: flex;
      flex-direction: column;
      justify-content: flex-start;
@@ -96,23 +104,82 @@
   }
  .about-content span{
     color: #01011a;
-    font-size: 2rem;
-    font-weight: 700;
+    font-size: 2.5rem;
+    font-weight: 900;
+    text-transform: capitalize;
  }
+
+ .social-icon a{
+     display: inline-flex;
+     width: 50px;
+     height: 100%;
+     justify-content: center;
+     align-items: center;
+     border: 2px solid #01011a;
+     padding: 10px 10px;
+     box-shadow:  0 0 .3rem #01011a;
+     transition: .6s;
+     position: relative;
+     z-index: 1;
+     overflow: hidden;
+     border-radius: 50%;
+     margin-right: 1rem;
+     color: #01011a;
+  }
+
+  .social-icon a::before{
+   content: '';
+   position: absolute;
+   top: 0;
+   left: 0;
+   width: 0;
+   height: 100%;
+   background-color: #01011a;
+   z-index: -1;
+   transition: .4s;
+ }
+
+ .social-icon a:hover::before{
+   width: 100%;
+ }
+ .social-icon a:hover{
+   box-shadow: 0 0 2px 2px #01011a;
+   color: yellow;
+   border: 1px solid transparent;
+ }
+  .social-icon i{
+   font-size: 1.5rem;
+  }
+
+  .social-icon a:hover{
+     background-color: #01011a;
+  }
+ /* closed */
 
  .about-info{
     position: absolute;
     width: 100px;
     height: 100px;
     padding: 10px;
-    background-color: #01011a;
-    background-color: yellow;
-    color: #d0d0d0;
     font-size: 1rem;
     border-radius: 70% 30% 30% 70% / 30% 40% 30% 40%;
     animation: mother 6s linear infinite;
  }
 
+  .info-1{
+     position: absolute;
+     background-color: #01011a;
+     top: 40%;
+     right: -15%;
+     color: yellow;
+  }
+
+  .info-2{
+    position: absolute;
+    bottom: 5%;
+    background-color: #01011a;
+    color: yellow;
+  }
 
  @keyframes mother {
     0%, 100%{
